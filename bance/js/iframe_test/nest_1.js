a1271f66ffb14fc0c68038978cf521fddaf730e9
k-oka <k-oka@dimage.co.jp>
2025-06-26 17:55:00 +0900

    fix

--- bance/js/iframe_test/nest_1.js
+++ bance/js/iframe_test/nest_1.js
@@ -3,17 +3,29 @@ const js2path = "https://ds-k-oka.github.io/bance/js/iframe_test/nest_2.js";
 
 const midIframe = document.createElement("iframe");
 midIframe.width = "468";
-midIframe.height = "120"; // 高さを確保
-midIframe.scrolling = "yes"; // 常にスクロールバー表示
+midIframe.height = "90"; // ←この高さ内に収める
+midIframe.scrolling = "no";
+midIframe.style.border = "none";
 document.body.appendChild(midIframe);
 
 midIframe.addEventListener("load", () => {
   const midDoc = midIframe.contentDocument;
 
+  // bodyに相対位置指定
+  midDoc.body.style.position = "relative";
+  midDoc.body.style.margin = "0";
+  midDoc.body.style.padding = "0";
+  midDoc.body.style.overflow = "hidden";
+
   const innerIframe = midDoc.createElement("iframe");
   innerIframe.width = "468";
-  innerIframe.height = "100"; // 高さを確保
-  innerIframe.scrolling = "yes"; // スクロールバー表示
+  innerIframe.height = "90"; // 親と同じ高さ
+  innerIframe.scrolling = "yes";
+  innerIframe.style.position = "absolute"; // 親iframe内の左上に固定
+  innerIframe.style.top = "0";
+  innerIframe.style.left = "0";
+  innerIframe.style.border = "none";
+
   midDoc.body.appendChild(innerIframe);
 
   innerIframe.addEventListener("load", () => {
@@ -32,7 +44,7 @@ midIframe.addEventListener("load", () => {
     body.appendChild(script);
   });
 
-  innerIframe.src = 'about:blank';
+  innerIframe.src = "about:blank";
 });
 
-midIframe.src = 'about:blank';
+midIframe.src = "about:blank";
